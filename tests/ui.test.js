7f680894b10a53a51cb68de6a4831410b6ea97c8
Ivan Pekov <64958860+tanatis@users.noreply.github.com>
2024-03-09 13:05:38 +0200

    fixed test verify All Books link is visible

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -2,4 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test("Verify All Books link is visible", async ({page}) => {
     await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar');
+
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isLinkVisible = await allBooksLink.isVisible();
+
+    expect(isLinkVisible).toBe(true);
+
+test('')
 });
