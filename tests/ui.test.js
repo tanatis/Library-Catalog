9a89eba98a196c43bcd8253a01f9b09a60ee3ebb
Ivan Pekov <64958860+tanatis@users.noreply.github.com>
2024-03-09 13:21:32 +0200

    Added tests for login and register buttons

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,13 +1,31 @@
 const { test, expect } = require('@playwright/test');
 
+const baseUrl = 'http://localhost:3000';
+
 test("Verify All Books link is visible", async ({page}) => {
-    await page.goto('http://localhost:3000');
+    await page.goto(baseUrl);
     await page.waitForSelector('nav.navbar');
 
     const allBooksLink = await page.$('a[href="/catalog"]');
     const isLinkVisible = await allBooksLink.isVisible();
 
     expect(isLinkVisible).toBe(true);
+});
 
-test('')
+test('Verify Login button is visible', async ( {page} ) => {
+    await page.goto(baseUrl);
+    await page.waitForSelector('#guest');
+    const loginButton = await page.$('a.button:nth-child(1)');
+    const isLoginButtonVisible = await loginButton.isVisible();
+
+    expect(isLoginButtonVisible).toBe(true);
+});
+
+test('Verify Register button is visible', async ( {page} ) => {
+    await page.goto(baseUrl);
+    await page.waitForSelector('#guest');
+    const registerButton = await page.$('a.button:nth-child(2)');
+    const isRegisterButtonVisible = await registerButton.isVisible();
+
+    expect(isRegisterButtonVisible).toBe(true);
 });
